0e1dcda7640f574b82ba693afecc4fec469ebe48
skylak3 <guremorir@gmail.com>
2025-04-21 12:14:30 +0300

    fix

--- frontend/config-overrides.js
+++
@@ -1,6 +0,0 @@
-module.exports = function override(config, env) {
-    if (config.devServer) {
-      config.devServer.allowedHosts = ["localhost"]; // Разрешить все хосты
-    }
-    return config;
-  };
