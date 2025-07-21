095b80c1cf790e56b197ad9f9ade1b7728a65533
skylak3 <guremorir@gmail.com>
2025-07-21 10:59:07 +0300

    обновил фронт

--- backend/src/main.go
+++ backend/src/main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	_ = godotenv.Load(".env")
-	fmt.Println("WEATHER_API_URL:", os.Getenv("WEATHER_API_URL"))
+	//fmt.Println("WEATHER_API_URL:", os.Getenv("WEATHER_API_URL"))
 	router := gin.Default()
 	router.GET("/weather", getWeather)
 	router.Run(":8080")
